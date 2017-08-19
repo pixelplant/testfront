1fb1f1dfa2f2049dfe93393005048021daf19957
Radu Mogos <rmogos@wideagency.com>
2017-08-19 04:05:22 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        watch: {
+            // if any .scss file changes in directory "/scss" run the "sass"-task.
+            files: "scss/**/*.scss",
+            tasks: ["sass"]
+        },
+        // "sass"-task configuration
+        sass: {
+            // production config is also available
+            dist: {
+                files: {
+                    'css/ui.css': 'scss/ui.scss',
+                }
+            }
+        }
+    });
+    // the default task (running "grunt" in console) is "watch"
+    grunt.registerTask('default', ['watch']);
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+** Frontend test **
+
+npm install to install the required node modules
+
+grunt in the current folder to automatically build scss files
